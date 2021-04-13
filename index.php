24ded9cde7620cef785f337f81cf46d17a4830f3
Pataki Szilárd <pataki.szilard@gmail.com>
2021-04-13 09:20:11 +0200

    conclusion

--- index.php
+++ index.php
@@ -5,41 +5,38 @@
   </head>
   <body>
     <?php
-      $name = "PHP Store";
-      $credit = 1000;
+	    $name = "PHP Store";
+			$credit = 1000;
 
-      echo "<h1>Welcome to ".$name."!</h1>";
-      echo "<h2>You have $".$credit." in your wallet.</h2>";
+	    echo "<h1>Welcome to ".$name."!</h1>";
+	    echo "<h2>You have $".$credit." in your wallet.</h2>";
 
-	    $products['Computer']=750;
+		  $products['Computer']=750;
 	    $products['Car']=15000;
 	    $products['iPhone']=1000;
 	    $products['Toaster']=75;
 
+      $taxRate=0.0825;
+
+      function tax_calc($amount,$tax){
+	    	$addedTax = $amount*$tax;
+	    	$amountWithTax = round($amount+$addedTax,2);
+	    	return $amountWithTax;
+    	}
+
 	    foreach($products as $key => $value){
-		    echo "<p>The ".$key." costs ".$value."</p>";
+	    	$costWithTax = tax_calc($value,$taxRate);
+		    echo "<p>The ".$key." costs ".$costWithTax." with tax</p>";
 	    }
 
-        echo "<h2>Items you can afford</h2>";
+	    echo "<h2>Items you can afford</h2>";
 
 	    foreach($products as $key => $value){
-		    if($value <= $credit ){
+	    	$costWithTax = tax_calc($value,$taxRate);
+		    if($costWithTax <= $credit ){
 		    	echo "<p>".$key."</p>";
 		    }
 	    }
-
-        $amount=800;
-        $taxRate=0.0825;
-        $addedTax= $amount*$taxRate;  //amount = 800, tax = .0825
-        echo "<p>".$addedTax."</p>";
-
-        echo "<p>".tax_calc(750,0.223)."</p>"; // When you refresh your page you should the value 917.25 at the bottom of the screen.
-
-        function tax_calc($amount,$tax){
-            $calculate_tax = $amount*$tax;
-            $amount = round($amount+$calculate_tax,2);
-            return $amount;
-        }
     ?>
   </body>
 </html>
